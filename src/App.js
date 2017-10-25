92c11d734273939e1a7b05d2c7ba8f3739cef8a9
David Nguyen <d_nguyen@berkeley.edu>
2017-10-25 14:34:34 -0700

    add rudimentary ajax calls to main component

--- src/App.js
+++ src/App.js
@@ -4,71 +4,21 @@ import './App.css';
 import LifeSignList from './components/LifeSignList';
 import LifeSignMap from './components/LifeSignMap';
 
+const URL = '';
 class App extends Component {
   constructor(props) {
     super(props);
-
-    let exampleData = [
-      {
-        bssid: '4A:5D:4C:EA:64:52',
-        station: 'B4:99:BA:0A:87:DE',
-        pwr: '-30',
-        rate: '0 - 1',
-        lost: '1',
-        frames: '2',
-        probe: '',
-        lastSeen: 1508823732088,
-      },
-      {
-        bssid: 'BC:14:01:F2:F6:28',
-        station: '18:5E:0F:F2:EB:76',
-        pwr: '-55',
-        rate: '0 - 2e',
-        lost: '0',
-        frames: '1',
-        probe: '',
-        lastSeen: 1508823742378,
-      },
-      {
-        bssid: 'BC:14:01:F2:F6:28',
-        station: '18:B4:30:2A:24:42',
-        pwr: '-90',
-        rate: '0 - 2',
-        lost: '0',
-        frames: '1',
-        probe:'',
-        lastSeen: 1508823751861,
-        name:'Richard',
-      },
-      {
-        bssid: 'BC:14:01:F2:F6:28',
-        station: '18:B4:30:2A:24:33',
-        pwr: '-100',
-        rate: '0 - 2',
-        lost: '0',
-        frames: '1',
-        probe:'',
-        lastSeen: 1508823751861,
-        name:'Woah',
-      },
-    ];
-    
-    // wew
-    for (let lifeSign in exampleData) {
-      let minutesAgo = Math.floor((new Date().getTime() - new Date(exampleData[lifeSign].lastSeen)) / 1000 / 60);
-      if (minutesAgo > 60) {
-        let hoursAgo = Math.floor(minutesAgo / 60);
-        exampleData[lifeSign].lastSeen = `${hoursAgo} hour${hoursAgo !== 1 ? 's' : ''} ago`;
-      } else if (minutesAgo < 5) {
-        exampleData[lifeSign].lastSeen = 'Active Now';
-      } else {
-        exampleData[lifeSign].lastSeen = `${minutesAgo} minutes ago`;
-      }
-    }
-
     this.state = {
-      lifeSigns: exampleData,
+      lifeSigns: null,
     }
+  }
+
+  componentDidMount() {
+    this.fetchData().then((res) => {
+      this.setState({
+        lifeSigns: this.processData(res),
+      });
+    });
   }
 
   toggleState() {
@@ -134,24 +84,56 @@ class App extends Component {
     });
   }
 
+  fetchData() {
+    // fetch(URL)
+    //   .then(res => res.json());
+    return new Promise((res, rej) => {
+      res({"status":200,"clients":{"B4:99:BA:0A:87:DE":{"bssid":"4A:5D:4C:EA:64:52","station":"B4:99:BA:0A:87:DE","pwr":"-81","rate":"0 - 1","lost":"1","frames":"2","probe":"","lastSeen":1508823732088},"18:5E:0F:F2:EB:76":{"bssid":"BC:14:01:F2:F6:28","station":"18:5E:0F:F2:EB:76","pwr":"-67","rate":"0 - 2e","lost":"0","frames":"1","probe":"","lastSeen":1508823742378},"18:B4:30:2A:24:42":{"bssid":"BC:14:01:F2:F6:28","station":"18:B4:30:2A:24:42","pwr":"-68","rate":"0 - 2","lost":"0","frames":"1","probe":"","lastSeen":1508823751861, "name":"Richard"}}});
+    })
+  }
+
+  processData(res) {
+    let data = Object.keys(res.clients).map(key => res.clients[key]);
+    
+    // wew
+    for (let lifeSign in data) {
+      let minutesAgo = Math.floor((new Date().getTime() - new Date(data[lifeSign].lastSeen)) / 1000 / 60);
+      if (minutesAgo > 60) {
+        let hoursAgo = Math.floor(minutesAgo / 60);
+        data[lifeSign].lastSeen = `${hoursAgo} hour${hoursAgo !== 1 ? 's' : ''} ago`;
+      } else if (minutesAgo < 5) {
+        data[lifeSign].lastSeen = 'Active Now';
+      } else {
+        data[lifeSign].lastSeen = `${minutesAgo} minutes ago`;
+      }
+    }
+
+    return data;
+  }
+
+
   render() {
-    return (
-      <div className="Container">
-        <div className="LifeSignListContainer">
-          <LifeSignList
-            lifeSigns={this.state.lifeSigns} 
-          />
-        </div>
-        <div className="LifeSignMapContainer">
-          <LifeSignMap 
-            lifeSigns={this.state.lifeSigns}
+
+    if (!this.state.lifeSigns) { return (<div>Loading...</div>); }
+    else {
+      return (
+        <div className="Container">
+          <div className="LifeSignListContainer">
+            <LifeSignList
+              lifeSigns={this.state.lifeSigns} 
             />
+          </div>
+          <div className="LifeSignMapContainer">
+            <LifeSignMap 
+              lifeSigns={this.state.lifeSigns}
+              />
+          </div>
+          <div className="RightSidebar">
+            <div className="Button" style={{color: '#fff'}} onClick={() => this.toggleState()}>Hello</div>
+          </div>
         </div>
-        <div className="RightSidebar">
-          <div className="Button" style={{color: '#fff'}} onClick={() => this.toggleState()}>Hello</div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
